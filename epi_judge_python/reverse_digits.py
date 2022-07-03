08473d91e8d03a85e7a7c0f93098c9f7bcb78616
Nuruzzaman Milon <contact@milon.im>
2022-07-03 21:51:10 +0200

    4.08: reverse digits

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -1,9 +1,17 @@
 from test_framework import generic_test
 
+"""
+Time Complexity: O(n)
+Space Complexity: O(1)
+Hint: get the last digit of x, then add it to the result. And then divide x by 10. Then do the same thing again until x is 0.
+"""
 
 def reverse(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    res, x_remaining = 0, abs(x)
+    while x_remaining:
+        res = res * 10 + x_remaining % 10
+        x_remaining //= 10
+    return -res if x < 0 else res
 
 
 if __name__ == '__main__':
