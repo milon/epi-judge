3965a88c58bd3c3cd7e38157e3184d4f56560762
Nuruzzaman Milon <contact@milon.im>
2022-07-03 22:32:00 +0200

    4.10: generate uniform random numbers

--- epi_judge_python/uniform_random_number.py
+++ epi_judge_python/uniform_random_number.py
@@ -10,10 +10,24 @@ from test_framework.test_utils import enable_executor_hook
 def zero_one_random():
     return random.randrange(2)
 
+"""
+Time complexity: O(log(b - a + 1))
+Space complexity: O(1)
+Hint: Use the fact that the uniform random number is in the range [a, b]. It is easy to produce a random integerbetween 0 and 2^i-1, 
+inclusive: concatenate I bits produced by the random number generator. For example, two calls to the random number generator will produce 
+one of (00)2, (01)2, (10)2, (11)2. These four possible outcomes encode the four integers 0,1,2,3, and all of them are equally likely.
+"""
 
 def uniform_random(lower_bound: int, upper_bound: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    num_of_outcomes = upper_bound - lower_bound + 1
+    while True:
+        res, i = 0, 0
+        while (1 << i) < num_of_outcomes:
+            res = (res << 1) | zero_one_random()
+            i += 1
+        if res < num_of_outcomes:
+            break
+    return res + lower_bound
 
 
 @enable_executor_hook
