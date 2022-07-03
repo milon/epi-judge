53d4dd389c0b93da40c8a8f89b26eb396b971b8b
Nuruzzaman Milon <contact@milon.im>
2022-07-03 15:32:26 +0200

    4.03: reverse bits

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,9 +1,17 @@
 from test_framework import generic_test
 
+"""
+Time Complexity: O(1)
+Space Complexity: O(1)
+Hint: We check the last bit of x and then replace it with first bit of the result. We will do this for all 64 bits of x.
+"""
 
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    res = 0
+    for i in range(64):
+        bit = (x >> i) & 1
+        res |= bit << (63 - i)
+    return res
 
 
 if __name__ == '__main__':
