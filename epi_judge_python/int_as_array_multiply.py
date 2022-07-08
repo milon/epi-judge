8a8b88c32b5233b8ff4acf00ad7f3afaadfa84aa
Nuruzzaman Milon <contact@milon.im>
2022-07-08 21:11:18 +0200

    5.03: Multiply two arbitrary-precision integers

--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -2,10 +2,23 @@ from typing import List
 
 from test_framework import generic_test
 
+"""
+Time complexity: O(n*m)
+Space complexity: O(n+m)
+"""
 
 def multiply(num1: List[int], num2: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    sign = -1 if (num1[0] < 0) ^ (num2[0] < 0) else 1
+    num1[0], num2[0] = abs(num1[0]), abs(num2[0])
+
+    result = [0] * (len(num1) + len(num2))
+    for i in reversed(range(len(num1))):
+        for j in reversed(range(len(num2))):
+            result[i + j + 1] += num1[i] * num2[j]
+            result[i + j] += result[i + j + 1] // 10
+            result[i + j + 1] %= 10
+    result = result[next((i for i in range(len(result)) if result[i] != 0), len(result)):] or [0]
+    return [sign * result[0]] + result[1:]
 
 
 if __name__ == '__main__':
