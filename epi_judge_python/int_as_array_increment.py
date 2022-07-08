66c4a1d9606e2b2b931c6f36cc3813c7a8ece204
Nuruzzaman Milon <contact@milon.im>
2022-07-08 14:00:12 +0200

    5.02: Increment an arbitrary precision int

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -2,10 +2,22 @@ from typing import List
 
 from test_framework import generic_test
 
+"""
+Time complexity: O(n)
+Space complexity: O(1)
+"""
 
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i - 1] += 1
+    if A[0] == 10:
+        A[0] = 1
+        A.append(0)
+    return A
 
 
 if __name__ == '__main__':
