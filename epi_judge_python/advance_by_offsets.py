d41293c75fdf71f5025907c7816b6c90754399e3
Nuruzzaman Milon <contact@milon.im>
2022-07-08 21:15:20 +0200

    5.04: advance through array

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -2,10 +2,18 @@ from typing import List
 
 from test_framework import generic_test
 
+"""
+Time complexity: O(n)
+Space complexity: O(1)
+"""
 
 def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
-    return True
+    furtherest_step, last_index = 0, len(A) - 1
+    i = 0
+    while i <= furtherest_step and i < last_index:
+        furtherest_step = max(furtherest_step, A[i] + i)
+        i += 1
+    return furtherest_step >= last_index
 
 
 if __name__ == '__main__':
